e955e6b18e16eae2e7804fdf9ac29d30bcd2dee4
Koodies <kwtan@outlook.sg>
2022-06-07 14:14:12 +0800

    MV array check and sort into query & option

--- src/controllers/launchpad.js
+++ src/controllers/launchpad.js
@@ -3,21 +3,14 @@ const fetch = require("node-fetch");
 module.exports = {
   async getFailedLaunches(launchpadID) {
     const launchpad = await getLaunchPadName(launchpadID);
-    const datas = await getLaunchesData(launchpadID);
+    const datas = await getLaunchDocs(launchpadID);
     let all_failures = [];
     datas.forEach((data) => {
-      if (data["failures"].length != 0) {
-        const failures = [data["failures"][0].reason];
-        all_failures.push({
-          id: data["id"],
-          name: data["name"],
-          failures,
-        });
-      }
-    });
-    all_failures.sort((a, b) => (b["id"] > a["id"] ? 1 : -1));
-    all_failures.map((data) => {
-      delete data["id"];
+      const failures = data["failures"].map((failure) => failure.reason);
+      all_failures.push({
+        name: data["name"],
+        failures,
+      });
     });
     return {
       launchpad,
@@ -34,13 +27,19 @@ async function getLaunchPadName(launchpadID) {
   return data.name;
 }
 
-async function getLaunchesData(launchpadID) {
+async function getLaunchDocs(launchpadID) {
   const query = {
-    id: launchpadID,
+    launchpad: launchpadID,
+    failures: { $exists: true, $not: { $size: 0 } },
+  };
+  const options = {
+    sort: {
+      _id: "desc",
+    },
   };
   const response = await fetch("https://api.spacexdata.com/v4/launches/query", {
     method: "post",
-    body: JSON.stringify(query),
+    body: JSON.stringify({ query, options }),
     headers: { "Content-Type": "application/json" },
   });
   const data = await response.json();
